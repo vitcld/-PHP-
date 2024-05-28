2c7dcf3f4ea1a3747f45ceb734454245082fb3b7
Victor Claudino <86791443+vitcld@users.noreply.github.com>
2024-05-27 21:18:25 -0300

    Delete Site Bolsa directory

--- Site Bolsa/imaginaria/includes/conectarBDPDO.php
+++
@@ -1,4 +0,0 @@
-<?php
-	//Conexao com a base de dados
-	$conexaoPDO = new PDO("mysql:dbname=imaginariaD4;host=127.0.0.1:3306","root","");
-?>
--- Site Bolsa/imaginaria/js/configMenus.js
+++
@@ -1,9 +0,0 @@
-//Ativar Menu Dropdown
-const elemsDropdown = document.querySelectorAll(".dropdown-trigger");
-const instacesDropdown = M.Dropdown.init(elemsDropdown,
-    {
-        coverTrigger:false
-    } );
-//Ativar o menu mobile
-elemsSidenav = document.querySelectorAll(".sidenav");
-const instancesSidenav = M.Sidenav.init(elemsSidenav);
